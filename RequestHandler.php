9da680f4e99fb97a97116e48b29ca8705cb82662
Paul Simpson <paulnsimpson@gmail.com>
2014-07-17 13:53:43 +0100

    Class for handling back end requests

---
+++ RequestHandler.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Class RequestHandler
+ * Processes all request to the app
+ */
+class RequestHandler
+{
+	//The action being taken
+	protected $action;
+
+	//Any GET/POST parameters supplied with the request
+	protected $params;
+
+	public function __construct(array $request)
+	{
+		$this->parseRequest($request);
+	}
+
+	/**
+	 * Parses the request
+	 * @param $request
+	 */
+	private function parseRequest($request)
+	{
+
+		if(isset($request['action']))
+		{
+			$this->action = $request['action'];
+			unset($request['action']);
+		}
+
+		$this->params = $request;
+	}
+
+	/**
+	 * Calls a method based on the request action parameter
+	 * @param null $action
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function callAction($action = null)
+	{
+		$method = $action ?: $this->action;
+
+		if(is_null($method))
+		{
+			throw new Exception('No action supplied!');
+		}
+
+		return $this->{$method}();
+	}
+
+	/**
+	 * Handles a conversion request when a form is submitted
+	 * with a Formisimo integration
+	 * @return bool
+	 */
+	public function trackConversion()
+	{
+		$rawRequest = $this->decodeParam('rawRequest');
+
+		$transporter = new ConversionTransporter($rawRequest);
+
+		return $transporter->run();
+	}
+
+	/**
+	 * Handles a remove integration request. For when
+	 * a Jotform user removes the Formisimo integration from form
+	 * @return bool
+	 * @throws Exception
+	 */
+	public function removeIntegration()
+	{
+		$formID = $this->getParam('formID');
+		$apiKey = $this->getParam('apiKey');
+
+		if ( ! $apiKey or ! $formID)
+		{
+			throw new Exception('Api Key and Form ID are required.');
+		}
+
+		$jotform = new JotForm($apiKey);
+
+		$destroyer = new WebhookDestroyer($jotform, $formID);
+		return $destroyer->run();
+	}
+
+	/**
+	 * Retrieves a request parameter
+	 * @param $key
+	 * @param null $default
+	 * @return null
+	 */
+	protected  function getParam($key, $default = null)
+	{
+		if(isset($this->params[$key]))
+		{
+			return $this->params[$key];
+		}
+
+		return $default;
+	}
+
+	/**
+	 * Find a request parameter and run it through json_decode
+	 * @param $key
+	 * @param array $default
+	 * @return array|mixed
+	 */
+	private function decodeParam($key, $default = array())
+	{
+		$param = $this->getParam($key);
+
+		if(is_null($param)) return $default;
+
+		$data = json_decode($this->stripJSON($param), true);
+
+		if ( ! is_null($data)) return $data;
+
+		$data = json_decode($param, true);
+
+		return is_null($data) ? $default : $data;
+	}
+
+	/**
+	 * Strips extra slashes in JSON
+	 * Added because of weird formatting in request data.
+	 * If I can figure out the reason for that we can remove this function
+	 * @param $str
+	 * @return mixed
+	 */
+	private function stripJSON($str)
+	{
+		return str_replace("\\'", "'", str_replace('\\"', '"', $str));
+	}
+
+	/**
+	 * Throws exception for any unknown request action
+	 * @param $method
+	 * @param $parameters
+	 * @throws Exception
+	 */
+	public function __call($method, $parameters)
+	{
+		throw new Exception('Unknown Action.');
+	}
+}
